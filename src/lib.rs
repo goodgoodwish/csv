4659cb876a955d3cbae06fbcc0d121e0c7c104b9
Yi Zhu <charlieyi@fb.com>
2022-10-16 20:11:14 -0700

    print_result

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use anyhow::{anyhow, bail, Result};
 use csv::Reader;
 use serde::Deserialize;
+use std::collections::{HashMap, HashSet};
 use std::env;
-use std::collections::HashMap;
 
 type Bal = HashMap<usize, Balance>;
 
@@ -19,22 +19,31 @@ pub fn run() -> Result<()> {
 fn process_tx(txs: &[Tx]) -> Result<()> {
     let mut bal: Bal = HashMap::new();
     let mut tx_amt: HashMap<usize, f64> = HashMap::new();
+    let mut dispute_txs: HashSet<usize> = HashSet::new();
 
     for tx in txs {
         match &tx.tx_type[..] {
             "deposit" => deposit(&tx, &mut bal, &mut tx_amt)?,
             "withdrawal" => withdraw(&tx, &mut bal, &mut tx_amt)?,
+            "dispute" => dispute(&tx, &mut bal, &mut tx_amt, &mut dispute_txs)?,
+            "resolve" => resolve(&tx, &mut bal, &mut tx_amt, &mut dispute_txs)?,
+            "chargeback" => chargeback(&tx, &mut bal, &mut tx_amt, &mut dispute_txs)?,
             _ => (),
         }
     }
 
-    println!("bal {bal:?}\n tx_amt {tx_amt:?}");
+    // println!("bal {bal:?}\n tx_amt {tx_amt:?}");
+    print_result(&bal)?;
 
     Ok(())
 }
 
 fn deposit(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
     println!("deposit {tx:?}");
+    if tx_amt.contains_key(&tx.tx_id) {
+        println!("TX {} already applied", tx.tx_id);
+        return Ok(());
+    }
     let client = tx.client;
     if !bal.contains_key(&client) {
         println!("Cleint {} not exists", client);
@@ -43,6 +52,10 @@ fn deposit(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<(
     // let client_bal = bal.get_mut(&client).unwrap();
     // client_bal.available += tx.amount;
     let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if client_data.locked {
+        println!("Cleint {} is locked, return", client);
+        return Ok(());
+    }
     client_data.available += tx.amount;
     tx_amt.insert(tx.tx_id, tx.amount);
     Ok(())
@@ -50,12 +63,21 @@ fn deposit(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<(
 
 fn withdraw(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
     println!("withdraw {tx:?}");
+    if tx_amt.contains_key(&tx.tx_id) {
+        println!("TX {} already applied", tx.tx_id);
+        return Ok(());
+    }
     let client = tx.client;
     if !bal.contains_key(&client) {
         println!("Warning! Cleint {} not exists", client);
         return Ok(());
     }
+
     let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if client_data.locked {
+        println!("Cleint {} is locked, return", client);
+        return Ok(());
+    }
     if client_data.available < tx.amount {
         println!("Warning! Cleint {} not enough balance", client);
         return Ok(());
@@ -65,25 +87,50 @@ fn withdraw(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<
     Ok(())
 }
 
-fn dispute(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+fn dispute(
+    tx: &Tx,
+    bal: &mut Bal,
+    tx_amt: &mut HashMap<usize, f64>,
+    dispute_txs: &mut HashSet<usize>,
+) -> Result<()> {
     println!("dispute {tx:?}");
     let client = tx.client;
     if !bal.contains_key(&client) {
         println!("Warning! Cleint {} not exists", client);
         return Ok(());
     }
-    let client_data = bal.entry(client).or_insert(Balance::new(client));
     if !tx_amt.contains_key(&tx.tx_id) {
         println!("Warning! tx {} not exists", tx.tx_id);
         return Ok(());
     }
+
+    let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if client_data.locked {
+        println!("Warning! Cleint {} is locked, return", client);
+        return Ok(());
+    }
+    if dispute_txs.contains(&tx.tx_id) {
+        println!("Warning! dispute tx {} already applied", tx.tx_id);
+        return Ok(());
+    }
+    dispute_txs.insert(tx.tx_id);
     client_data.available -= tx_amt[&tx.tx_id];
     client_data.held += tx_amt[&tx.tx_id];
     Ok(())
 }
 
-fn resolve(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+fn resolve(
+    tx: &Tx,
+    bal: &mut Bal,
+    tx_amt: &mut HashMap<usize, f64>,
+    dispute_txs: &mut HashSet<usize>,
+) -> Result<()> {
     println!("resolve {tx:?}");
+    if !dispute_txs.contains(&tx.tx_id) {
+        println!("Warning! dispute tx {} not exists", tx.tx_id);
+        return Ok(());
+    }
+
     let client = tx.client;
     if !bal.contains_key(&client) {
         println!("Warning! Cleint {} not exists", client);
@@ -96,11 +143,22 @@ fn resolve(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<(
     }
     client_data.available += tx_amt[&tx.tx_id];
     client_data.held -= tx_amt[&tx.tx_id];
+    dispute_txs.remove(&tx.tx_id);
     Ok(())
 }
 
-fn chargeback(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+fn chargeback(
+    tx: &Tx,
+    bal: &mut Bal,
+    tx_amt: &mut HashMap<usize, f64>,
+    dispute_txs: &mut HashSet<usize>,
+) -> Result<()> {
     println!("chargeback {tx:?}");
+    if !dispute_txs.contains(&tx.tx_id) {
+        println!("Warning! dispute tx {} not exists", tx.tx_id);
+        return Ok(());
+    }
+
     let client = tx.client;
     if !bal.contains_key(&client) {
         println!("Warning! Cleint {} not exists", client);
@@ -132,6 +190,21 @@ fn input_filename() -> Result<String> {
     }
     let csv_file = args[1].clone();
     Ok(csv_file)
+}
+
+fn print_result(bal: &Bal) -> Result<()> {
+    println!("client, available, held, total, locked");
+    for (_, client_bal) in bal.into_iter() {
+        let Balance {
+            client,
+            available,
+            held,
+            locked,
+        } = *client_bal;
+        let total = available + held;
+        println!("{client}, {available}, {held}, {total}, {locked}");
+    }
+    Ok(())
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
