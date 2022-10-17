4c5c208b83ca13f1337f040d165d9492bde6ae8d
Yi Zhu <charlieyi@fb.com>
2022-10-16 19:20:31 -0700

    2022_10_06 draft all functions,

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use serde::Deserialize;
 use std::env;
 use std::collections::HashMap;
 
+type Bal = HashMap<usize, Balance>;
+
 pub fn run() -> Result<()> {
     let csv_file = input_filename()?;
     println!("csv_file {csv_file}");
@@ -15,22 +17,23 @@ pub fn run() -> Result<()> {
 }
 
 fn process_tx(txs: &[Tx]) -> Result<()> {
-    let mut bal: HashMap<usize, Balance> = HashMap::new();
+    let mut bal: Bal = HashMap::new();
+    let mut tx_amt: HashMap<usize, f64> = HashMap::new();
 
     for tx in txs {
         match &tx.tx_type[..] {
-            "deposit" => deposit(&tx, &mut bal)?,
-            "withdrawal" => withdraw(&tx, &mut bal)?,
+            "deposit" => deposit(&tx, &mut bal, &mut tx_amt)?,
+            "withdrawal" => withdraw(&tx, &mut bal, &mut tx_amt)?,
             _ => (),
         }
     }
 
-    println!("bal {bal:?}");
+    println!("bal {bal:?}\n tx_amt {tx_amt:?}");
 
     Ok(())
 }
 
-fn deposit(tx: &Tx, bal: &mut HashMap<usize, Balance>) -> Result<()> {
+fn deposit(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
     println!("deposit {tx:?}");
     let client = tx.client;
     if !bal.contains_key(&client) {
@@ -41,11 +44,75 @@ fn deposit(tx: &Tx, bal: &mut HashMap<usize, Balance>) -> Result<()> {
     // client_bal.available += tx.amount;
     let client_data = bal.entry(client).or_insert(Balance::new(client));
     client_data.available += tx.amount;
+    tx_amt.insert(tx.tx_id, tx.amount);
     Ok(())
 }
 
-fn withdraw(tx: &Tx, bal: &mut HashMap<usize, Balance>) -> Result<()> {
+fn withdraw(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
     println!("withdraw {tx:?}");
+    let client = tx.client;
+    if !bal.contains_key(&client) {
+        println!("Warning! Cleint {} not exists", client);
+        return Ok(());
+    }
+    let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if client_data.available < tx.amount {
+        println!("Warning! Cleint {} not enough balance", client);
+        return Ok(());
+    }
+    client_data.available -= tx.amount;
+    tx_amt.insert(tx.tx_id, -tx.amount);
+    Ok(())
+}
+
+fn dispute(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+    println!("dispute {tx:?}");
+    let client = tx.client;
+    if !bal.contains_key(&client) {
+        println!("Warning! Cleint {} not exists", client);
+        return Ok(());
+    }
+    let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if !tx_amt.contains_key(&tx.tx_id) {
+        println!("Warning! tx {} not exists", tx.tx_id);
+        return Ok(());
+    }
+    client_data.available -= tx_amt[&tx.tx_id];
+    client_data.held += tx_amt[&tx.tx_id];
+    Ok(())
+}
+
+fn resolve(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+    println!("resolve {tx:?}");
+    let client = tx.client;
+    if !bal.contains_key(&client) {
+        println!("Warning! Cleint {} not exists", client);
+        return Ok(());
+    }
+    let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if !tx_amt.contains_key(&tx.tx_id) {
+        println!("Warning! tx {} not exists", tx.tx_id);
+        return Ok(());
+    }
+    client_data.available += tx_amt[&tx.tx_id];
+    client_data.held -= tx_amt[&tx.tx_id];
+    Ok(())
+}
+
+fn chargeback(tx: &Tx, bal: &mut Bal, tx_amt: &mut HashMap<usize, f64>) -> Result<()> {
+    println!("chargeback {tx:?}");
+    let client = tx.client;
+    if !bal.contains_key(&client) {
+        println!("Warning! Cleint {} not exists", client);
+        return Ok(());
+    }
+    let client_data = bal.entry(client).or_insert(Balance::new(client));
+    if !tx_amt.contains_key(&tx.tx_id) {
+        println!("Warning! tx {} not exists", tx.tx_id);
+        return Ok(());
+    }
+    client_data.held += tx_amt[&tx.tx_id];
+    client_data.locked = true;
     Ok(())
 }
 
@@ -80,6 +147,7 @@ struct Balance {
     client: usize,
     available: f64,
     held: f64,
+    locked: bool,
 }
 
 impl Balance {
@@ -88,6 +156,7 @@ impl Balance {
             client,
             available: 0.0,
             held: 0.0,
+            locked: false,
         }
     }
 }
